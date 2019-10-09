7ee7577fcf2259d2401b84d114366fe39a2882ec
djfalciani <dfalciani@gmail.com>
2019-10-08 22:48:27 -0400

    adding some basic layout components  from class examples

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Nav />
+    //   <Router>
+    //   <div>
+    //     <Nav />
+    //     <Switch>
+    //       <Route exact path="/" component={Books} />
+    //       <Route exact path="/books" component={Books} />
+    //       <Route exact path="/books/:id" component={Detail} />
+    //       <Route component={NoMatch} />
+    //     </Switch>
+    //   </div>
+    // </Router>
     );
   }
 }
